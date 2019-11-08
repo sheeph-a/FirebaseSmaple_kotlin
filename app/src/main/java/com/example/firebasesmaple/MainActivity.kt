3b0c8649394a72b6a5198fac55210b0891ec84ee
sheeph-a <49810315+sheeph-a@users.noreply.github.com>
2019-11-08 11:50:36 +0900

    Add sign-in function

--- app/src/main/java/com/example/firebasesmaple/MainActivity.kt
+++ app/src/main/java/com/example/firebasesmaple/MainActivity.kt
@@ -3,10 +3,32 @@ package com.example.firebasesmaple
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import android.content.Intent
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
+
 class MainActivity : AppCompatActivity() {
+
+    var mFirebaseAuth : FirebaseAuth = null
+    var mFirebaseUser : FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mFirebaseAuth = FirebaseAuth.getInstance()
+        mFirebaseUser = mFirebaseAuth.getCurrentUser()
+        if (mFirebaseUser == null)
+            // Not signed in, launch the Sign In activity
+            startActivity(Intent(this, SignInActivity::class))
+            finish()
+            return
+
     }
 }
